a8d37cd4fe089defbd4091610253d1a6fde65497
SunDoge <384813529@qq.com>
2017-04-23 23:29:30 +0800

    add grid

--- ai.h
+++ ai.h
@@ -1,12 +1,46 @@
 #ifndef AI_H
 #define AI_H
+#include "gamewidget.h"
+#include <vector>
+
+struct Cell {
+    int x;
+    int y;
+};
+
+struct Result {
+    GestureDirect move;
+    int score;
+    Cell positions;
+    //cutoffs
+};
+
+class Grid {
+public:
+    Grid();
+    Grid(int array[4][4]);
+    
+private:
+    int cells[4][4];
+    vector<Cell> availableCells();
+    double smoothness();
+    double monotonicity2();
+    double maxValue();
+    
+};
 
 class AI
 {
 public:
     AI();
+    AI(int grid[4][4]);
 
-
+private:
+    //int grid[4][4];
+    Grid grid;
+    Result search(int, double, double, Cell);
+    Result iterativeDeep();
+    Result getBest();
 };
 
 #endif // AI_H
